f4e8413c63e9596cf17695de2dc0cea6cb292614
Anna <blinova.anneta@gmail.com>
2024-06-05 11:18:48 +0200

    update Header component

--- src/layout/header/Header.style.ts
+++ src/layout/header/Header.style.ts
@@ -1,8 +1,24 @@
+import { theme } from '../../components/styles/Theme';
+import { flexWrapper } from './../../components/styles/common';
 import styled from "styled-components";
 
 const Header = styled.header`
-  display: grid;
+  ${flexWrapper({gap: '20px'})};
+  background-color: ${theme.colors.bgBlock};
+  padding: 30px 20px;
+`
+const Menu = styled.ul`
+  ${flexWrapper({gap: '20px'})};
+`
+const Navigation = styled.nav`
+  ${flexWrapper({gap: '20px'})};
+`
+const UserMenu = styled.nav`
+  ${flexWrapper({gap: '20px'})};
 `
 export const style = {
-  Header
+  Header,
+  Menu,
+  Navigation,
+  UserMenu,
 }
--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,21 +2,25 @@ import logo from './../../assets/logotype.svg';
 import { style } from './Header.style.ts';
 import { SvgIcon } from '../../components/SvgIcon.tsx';
 import { UserElement } from '../../components/userElement/UserElement.tsx';
+import DropDownMenu from '../../components/dropDownMenu/DropDownMenu.tsx';
+import { dropDownMenuItems } from '../../data/data.ts';
+import { v1 } from 'uuid';
 
 export const Header = () => {
     return (
         <style.Header>
             <img src={logo} alt="logotype"/>
-            <nav>
-                <ul>
+            <style.Navigation>
+                <style.Menu>
                     <li><a href={'/'}><SvgIcon iconId={'news'}/></a></li>
                     <li><a href={'/'}><SvgIcon iconId={'message'}/></a></li>
                     <li><a href={'/'}><SvgIcon iconId={'notification'}/></a></li>
-                </ul>
-                <div>
-                    <UserElement/>
-                </div>
-            </nav>
+                </style.Menu>
+                <style.UserMenu>
+                    <UserElement id={v1()} name={'Настя Барашкова'} smallText={'@1234567890'} />
+                    <DropDownMenu menuItems={dropDownMenuItems}/>
+                </style.UserMenu>
+            </style.Navigation>
         </style.Header>
     );
 };
