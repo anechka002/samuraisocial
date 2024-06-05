475cebfb82e5327b5be2bb4f4924f4628cbf63ba
Anna <blinova.anneta@gmail.com>
2024-06-05 11:16:55 +0200

    created the types folder

---
+++ src/data/types.ts
@@ -0,0 +1,12 @@
+export type DropDownLinksType = {
+  href: string
+  title: string
+}
+
+export type PersonsDialogType = {
+  id: string
+  avatar?: string
+  name: string
+  smallText: string
+  isOnline?: boolean
+}
